b34cd6f4d7c1708fa0211b1af26fdeba299b400f
凉风有信 <3042146237@qq.com>
2021-08-28 10:48:03 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import './assets/style/base.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(Vant).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="header">
+      <img src="../assets/images/logo.png" class="header__img">
+      <span class="header__desc">服务中心预约系统</span>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+
+<style lang="scss" scoped>
+.wrapper{
+  background-color: #f1f1f1;
+}
+.header{
+  height: .4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &__img{
+    width: .32rem;
+    height: .32rem;
+    margin-right: .05rem;
+  }
+  &__desc{
+    font-size: .2rem;
+  }
+}
+</style>
